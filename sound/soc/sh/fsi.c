aef16fafbdf5ffa889cc35514df11c8ef139b2df
Kuninori Morimoto <morimoto.kuninori@renesas.com>
2009-09-11 10:32:50 +0900

    ASoC: Add capture support to FSI driver
    ** CAUTION **
    
    still buggy
    
    Signed-off-by: Kuninori Morimoto <morimoto.kuninori@renesas.com>

--- sound/soc/sh/fsi.c
+++ sound/soc/sh/fsi.c
@@ -532,6 +532,108 @@ static int fsi_data_push(struct fsi_priv *fsi)
 	return 0;
 }
 
+static void fsi_16data_pop(struct fsi_priv *fsi,
+			   struct snd_pcm_runtime *runtime,
+			   int recv)
+{
+	u16 *dma_start;
+	u32 rec;
+	int i;
+
+	/* get dma start position for FSI */
+	dma_start = (u16 *)runtime->dma_area;
+	dma_start += fsi->byte_offset / 2;
+
+	/*
+	 * soft dma
+	 * FSI can not use DMA when 16bpp
+	 */
+	for (i = 0; i < recv; i++) {
+		rec = fsi_reg_read(fsi, DIDT);
+		dma_start[i] = (u16)(rec >> 8);
+	}
+}
+
+static void fsi_32data_pop(struct fsi_priv *fsi,
+			   struct snd_pcm_runtime *runtime,
+			   int recv)
+{
+	u32 *dma_start;
+	int i;
+
+	/* get dma start position for FSI */
+	dma_start = (u32 *)runtime->dma_area;
+	dma_start += fsi->byte_offset / 4;
+
+	/*
+	dma_wait_for_completion(fsi->dma_chan);
+	dma_configure_channel(fsi->dma_chan, (DM_INC|0x400|TS_32|TM_BUR));
+	dma_read(fsi->dma_chan, (u32)(fsi->base + DIDT),
+		 (u32)dma_start, recv * 4);
+	*/
+
+	for (i = 0; i < recv; i++)
+		dma_start[i] = fsi_reg_read(fsi, DIDT);
+
+}
+
+static int fsi_data_pop(struct fsi_priv *fsi)
+{
+	struct snd_pcm_runtime *runtime;
+	struct snd_pcm_substream *substream = NULL;
+	int free;
+	int fifo_fill;
+	int width;
+
+	if (!fsi			||
+	    !fsi->substream		||
+	    !fsi->substream->runtime)
+		return -EINVAL;
+
+	runtime = fsi->substream->runtime;
+
+	/* FSI FIFO has limit.
+	 * So, this driver can not send periods data at a time
+	 */
+	if (fsi->byte_offset >=
+	    fsi->period_len * (fsi->periods + 1)) {
+
+		substream = fsi->substream;
+		fsi->periods = (fsi->periods + 1) % runtime->periods;
+
+		if (0 == fsi->periods)
+			fsi->byte_offset = 0;
+	}
+
+	/* get 1 channel data width */
+	width = frames_to_bytes(runtime, 1) / fsi->chan;
+
+	/* get free space for alsa */
+	free = (fsi->buffer_len - fsi->byte_offset) / width;
+
+	/* get recv size */
+	fifo_fill = fsi_get_fifo_residue(fsi, 0);
+
+	if (free < fifo_fill)
+		fifo_fill = free;
+
+	if (2 == width)
+		fsi_16data_pop(fsi, runtime, fifo_fill);
+	else if (4 == width)
+		fsi_32data_pop(fsi, runtime, fifo_fill);
+	else
+		return -EINVAL;
+
+	fsi->byte_offset += fifo_fill * width;
+
+	fsi_irq_enable(fsi, 0);
+
+	if (substream)
+		snd_pcm_period_elapsed(substream);
+
+	return 0;
+}
+
 static irqreturn_t fsi_interrupt(int irq, void *data)
 {
 	u32 status = fsi_master_read(SOFT_RST) & ~0x00000010;
@@ -545,6 +647,10 @@ static irqreturn_t fsi_interrupt(int irq, void *data)
 		fsi_data_push(&master->fsia);
 	if (int_st & INT_B_OUT)
 		fsi_data_push(&master->fsib);
+	if (int_st & INT_A_IN)
+		fsi_data_pop(&master->fsia);
+	if (int_st & INT_B_IN)
+		fsi_data_pop(&master->fsib);
 
 	fsi_master_write(INT_ST, 0x0000000);
 
@@ -699,16 +805,12 @@ static int fsi_dai_trigger(struct snd_pcm_substream *substream, int cmd,
 	int is_play = substream->stream == SNDRV_PCM_STREAM_PLAYBACK;
 	int ret = 0;
 
-	/* capture not supported */
-	if (!is_play)
-		return -ENODEV;
-
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
 		fsi_stream_push(fsi, substream,
 				frames_to_bytes(runtime, runtime->buffer_size),
 				frames_to_bytes(runtime, runtime->period_size));
-		ret = fsi_data_push(fsi);
+		ret = is_play ? fsi_data_push(fsi) : fsi_data_pop(fsi);
 		break;
 	case SNDRV_PCM_TRIGGER_STOP:
 		fsi_irq_disable(fsi, is_play);
@@ -845,7 +947,12 @@ struct snd_soc_dai fsi_soc_dai[] = {
 			.channels_min	= 1,
 			.channels_max	= 8,
 		},
-		/* capture not supported */
+		.capture = {
+			.rates		= FSI_RATES,
+			.formats	= FSI_FMTS,
+			.channels_min	= 1,
+			.channels_max	= 8,
+		},
 		.ops = &fsi_dai_ops,
 	},
 	{
@@ -857,7 +964,12 @@ struct snd_soc_dai fsi_soc_dai[] = {
 			.channels_min	= 1,
 			.channels_max	= 8,
 		},
-		/* capture not supported */
+		.capture = {
+			.rates		= FSI_RATES,
+			.formats	= FSI_FMTS,
+			.channels_min	= 1,
+			.channels_max	= 8,
+		},
 		.ops = &fsi_dai_ops,
 	},
 };
@@ -930,8 +1042,8 @@ static int fsi_probe(struct platform_device *pdev)
 		goto exit_free_dma;
 	}
 
-	fsi_soc_dai[0].dev		= &pdev->dev;
-	fsi_soc_dai[1].dev		= &pdev->dev;
+	fsi_soc_dai[0].dev	= &pdev->dev;
+	fsi_soc_dai[1].dev	= &pdev->dev;
 
 	fsi_soft_all_reset();
 
